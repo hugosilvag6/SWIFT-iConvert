8a46f9a309d6b2fe6b5798d79b1e90dfdb54c30a
hugosilvag6 <hugosilvag6@hotmail.com>
2022-06-20 11:34:04 -0300

    Initial Commit

---
+++ iConvert/iConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  iConvertApp.swift
+//  iConvert
+//
+//  Created by Hugo Silva on 20/06/22.
+//
+
+import SwiftUI
+
+@main
+struct iConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
